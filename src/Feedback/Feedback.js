eaa01115302c324f7577a636266d80fef7b97e07
Elena Gubarenko <lenagritsishina@gmail.com>
2021-04-23 17:40:16 +0300

    adding changes

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-// import nextId from 'react-id-generator';
+import nextId from 'react-id-generator';
 
 class Feedback extends Component {
   state = {
@@ -8,27 +8,51 @@ class Feedback extends Component {
     bad: 0,
   };
 
-  // buttonId = nextId();
+  buttonId = nextId();
 
-  method = () => {};
+  countTotalFeedback = () => {
+    const arrOfStateValues = Object.values(this.state);
+    const stateValuesSum = arrOfStateValues.reduce(
+      (acc, value) => acc + value,
+      0,
+    );
+    console.log(stateValuesSum);
+  };
+
+  addFeedbackCount = e => {
+    const targetName = e.target.name;
+
+    for (let key in this.state) {
+      if (targetName === key) {
+        this.setState(prevState => {
+          return { [key]: prevState[key] + 1 };
+        });
+      }
+    }
+    this.countTotalFeedback();
+  };
 
   render() {
     const props = this.props.feedbackCategories;
     const stateForRender = this.state;
     const stateDataForRender = Object.entries(stateForRender);
+    // props.map(prop => console.log(prop));
 
     return (
       <div>
         <h1>Please, leave feedback</h1>
         <ul>
           {props.map(prop => (
-            <button onClick={this.method}>{prop}</button>
+            <button name={prop} key={prop} onClick={this.addFeedbackCount}>
+              {prop}
+            </button>
           ))}
         </ul>
+
         <h2>Statistics</h2>
         <ul>
           {stateDataForRender.map(data => (
-            <li>
+            <li key={data}>
               {data[0]}: {data[1]}
             </li>
           ))}
